296daa8d5e4c6e855734ad3952d05367247eac05
Hanan Fayaz <hfayaz185@gmail.com>
2026-07-29 16:04:51 +0530

    Delete src directory

--- src/utils/normalizeStatus.ts
+++
@@ -1,9 +0,0 @@
-export const normalizeStatus = (status: string | number): string => {
-  if (typeof status === "number") {
-    return (
-      ["Draft", "Active", "Closed"][status] ?? "Unknown"
-    );
-  }
-
-  return status;
-};
